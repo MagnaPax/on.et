a44619a41a80dd0949fd6dcc137d5689891d18f1
ChunHee Han <chunhee.han@outlook.com>
2023-06-18 16:30:45 +0900

    feat: 모듈 생성 of elevation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ElevationModule } from './elevation/elevation.module';
 
 @Module({
-  imports: [],
+  imports: [ElevationModule],
   controllers: [AppController],
   providers: [AppService],
 })
