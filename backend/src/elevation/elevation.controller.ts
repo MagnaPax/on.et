7e2859522aeacdadc5b9aeb9a7f2821455ed0b4d
ChunHee Han <chunhee.han@outlook.com>
2023-06-18 17:03:03 +0900

    feat: 기본 Get for square

--- backend/src/elevation/elevation.controller.ts
+++ backend/src/elevation/elevation.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ElevationService } from './elevation.service';
 
 @Controller('elevation')
 export class ElevationController {
   constructor(private elevationService: ElevationService) {}
+
+  @Get('square')
+  getNumbers(@Query('row') nOfRaw: number, @Query('column') nOfColumn: number) {
+    return `${nOfRaw}, ${nOfColumn}`;
+  }
 }
