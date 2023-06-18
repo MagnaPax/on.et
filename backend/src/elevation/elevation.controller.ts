331f206494285a6593933ff11e484ec7a86f1542
ChunHee Han <chunhee.han@outlook.com>
2023-06-18 16:44:04 +0900

    feat: elevaton 서비스 객체 생성 into the controller

--- backend/src/elevation/elevation.controller.ts
+++ backend/src/elevation/elevation.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ElevationService } from './elevation.service';
 
 @Controller('elevation')
-export class ElevationController {}
+export class ElevationController {
+  constructor(private elevationService: ElevationService) {}
+}
