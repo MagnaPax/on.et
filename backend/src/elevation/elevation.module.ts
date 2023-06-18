4f9e3ce5c4dd5350e9db36b53082aa11fe21e0cd
ChunHee Han <chunhee.han@outlook.com>
2023-06-18 16:36:05 +0900

    feat: 서비스 생성 of elevation

--- backend/src/elevation/elevation.module.ts
+++ backend/src/elevation/elevation.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ElevationController } from './elevation.controller';
+import { ElevationService } from './elevation.service';
 
 @Module({
-  controllers: [ElevationController]
+  controllers: [ElevationController],
+  providers: [ElevationService]
 })
 export class ElevationModule {}
