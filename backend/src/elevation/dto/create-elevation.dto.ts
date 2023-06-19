e28655046730606b70e82076b6d34ace863424cd
ChunHee Han <chunhee.han@outlook.com>
2023-06-19 18:04:56 +0900

    feat: 쿼리스트링 유효성 검사

--- backend/src/elevation/dto/create-elevation.dto.ts
+++ backend/src/elevation/dto/create-elevation.dto.ts
@@ -1,18 +1,14 @@
-import { IsInt, Min, Max, ArrayUnique } from 'class-validator';
+import { Min, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateElevationDto {
-  @IsInt({ message: 'Column value must be an integer.' })
-  @Min(1, { message: 'Column value must be greater than or equal to 1.' })
-  @Max(Number.MAX_SAFE_INTEGER, { message: 'Column value is too large.' })
+  @IsNumber()
+  @Min(1)
+  @Type(() => Number)
   column: number;
 
-  @IsInt({ message: 'Row value must be an integer.' })
-  @Min(1, { message: 'Row value must be greater than or equal to 1.' })
-  @Max(Number.MAX_SAFE_INTEGER, { message: 'Row value is too large.' })
+  @IsNumber()
+  @Min(1)
+  @Type(() => Number)
   row: number;
-
-  @ArrayUnique({
-    message: 'ShownBlocks array cannot contain duplicate values.',
-  })
-  shownBlocks: number[];
 }
--- backend/src/elevation/elevation.controller.ts
+++ backend/src/elevation/elevation.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ElevationService } from './elevation.service';
+import { CreateElevationDto } from './dto/create-elevation.dto';
 
 @Controller('elevation')
 export class ElevationController {
   constructor(private elevationService: ElevationService) {}
 
   @Get('square')
-  getNumbers(@Query('row') nOfRaw: number, @Query('column') nOfColumn: number) {
-    // return `${nOfRaw}, ${nOfColumn}`;
-    return this.elevationService.getBlocks(nOfRaw, nOfColumn);
+  getNumbers(@Query() query: CreateElevationDto) {
+    // query 객체는 이미 유효성 검사 및 타입 변환을 완료
+    console.log(`${typeof query.row}, ${typeof query.column}`);
+    return `Row: ${query.row}, Column: ${query.column}`;
   }
 }
