d898edd8c711e942cd4e4224d6ba2859094d6257
ChunHee Han <chunhee.han@outlook.com>
2023-06-19 11:28:42 +0900

    feat: 쿼리스트링 유효성 검사

--- backend/src/elevation/dto/create-elevation.dto.ts
+++ backend/src/elevation/dto/create-elevation.dto.ts
@@ -1,5 +1,18 @@
+import { IsInt, Min, Max, ArrayUnique } from 'class-validator';
+
 export class CreateElevationDto {
+  @IsInt({ message: 'Column value must be an integer.' })
+  @Min(1, { message: 'Column value must be greater than or equal to 1.' })
+  @Max(Number.MAX_SAFE_INTEGER, { message: 'Column value is too large.' })
   column: number;
+
+  @IsInt({ message: 'Row value must be an integer.' })
+  @Min(1, { message: 'Row value must be greater than or equal to 1.' })
+  @Max(Number.MAX_SAFE_INTEGER, { message: 'Row value is too large.' })
   row: number;
+
+  @ArrayUnique({
+    message: 'ShownBlocks array cannot contain duplicate values.',
+  })
   shownBlocks: number[];
 }
