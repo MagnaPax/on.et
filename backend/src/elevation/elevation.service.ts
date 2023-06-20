33bdf9f2c49aa52dffac96172eb52709b21adb9d
ChunHee Han <chunhee.han@outlook.com>
2023-06-20 16:49:26 +0900

    feat: 각 사면의 입면도 숫자들 추출

--- backend/src/elevation/elevation.service.ts
+++ backend/src/elevation/elevation.service.ts
@@ -5,7 +5,36 @@ import { CreateElevationDto } from './dto/create-elevation.dto';
 export class ElevationService {
   private elevation: CreateElevationDto[] = [];
 
-  makeArray(row: number, column: number) {
+  getValuesOfSides(array: number[][]): number[][] {
+    const rowCount = array.length;
+    const columnCount = array[0].length;
+
+    const topVirtical: number[][] = Array.from(
+      { length: columnCount },
+      (_, index) => array.map((row) => row[index]),
+    );
+
+    const bottomVirtical: number[][] = [...topVirtical]
+      .reverse()
+      .map((row) => [...row].reverse());
+
+    const rightHorizon: number[][] = array.map((row) => row.reverse());
+
+    const leftHorizon: number[][] = [...rightHorizon]
+      .reverse()
+      .map((row) => [...row].reverse());
+
+    const newArray = [].concat(
+      topVirtical,
+      rightHorizon,
+      bottomVirtical,
+      leftHorizon,
+    );
+
+    return newArray;
+  }
+
+  makeArray(row: number, column: number): number[][] {
     const resultArray: number[][] = [];
 
     for (let i = 0; i < row; i++) {
@@ -22,8 +51,11 @@ export class ElevationService {
     return resultArray;
   }
 
-  async getBlocks(row: number, column: number) {
+  async getBlocks(row: number, column: number): Promise<number[][]> {
     const newArray = this.makeArray(row, column);
+
+    const theSides = this.getValuesOfSides(newArray);
+
     return await newArray;
   }
 }
